0ae48175e581dc1512e737a43b7e9c28f7d06ec5
Scoripo <aradusewicz@gmail.com>
2022-10-31 20:51:15 -0700

    fixed js to save multiple notes

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -6,19 +6,29 @@ var saveButton = document.querySelector("#save");
 renderLastRegistered();
 
 function renderLastRegistered(){
-    var notes = localStorage.getItem("notes");
+    $("#notes-list").empty();
+    var notes =JSON.parse(localStorage.getItem("notes"));
+    console.log("note", notes);
     if(!notes){
         return;
     }
+    for( var i=0; i<notes.length; i++) {
+        $("#notes-list").append($("<li>").text(notes[i]));
+    };
 noteInput.textContent = notes;
 console.log("mynotes " + notes);
-$("#notes-list").append($("<li>").text(notes));
 }
 
 saveButton.addEventListener("click", function(event){
     event.preventDefault();
-    var notes = document.querySelector("#notes").value;
-    localStorage.setItem("notes", notes);
+    var note = document.querySelector("#notes").value;
+    var notes = JSON.parse(localStorage.getItem("notes"));
+    if (!notes) {
+        notes = [];
+    }
+    
+    var note = document.querySelector("#notes").value;
+    notes.push(note+"");
+    localStorage.setItem("notes", JSON.stringify(notes));
     renderLastRegistered();
 });
-    
